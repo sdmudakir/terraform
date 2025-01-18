06abd01825b8d49ea365e4ce5caec1f4418de259
Ahmed <sdmudakir@gmail.com>
2025-01-18 16:05:25 +0300

    day-3

--- day-3-statefile/main.tf
+++ day-3-statefile/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "dev1" {
 ami = "ami-05576a079321f21f8"
   instance_type = "t2.micro"
   key_name = "devops"
-  availability_zone = "us-east-1b"
+  availability_zone = "us-east-1a"
   tags = {
   Name = "dev1"
   }
